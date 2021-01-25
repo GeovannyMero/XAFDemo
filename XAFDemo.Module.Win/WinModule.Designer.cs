144982c9421465ca042859b94fd9feca2dea5ea5
Geovanny Mero <jefferson_mero1911@hotmail.com>
2021-01-25 00:02:35 -0500

    Agregar archivos de proyecto.

---
+++ XAFDemo.Module.Win/WinModule.Designer.cs
@@ -0,0 +1,37 @@
+﻿namespace XAFDemo.Module.Win {
+    partial class XAFDemoWindowsFormsModule {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            // 
+            // XAFDemoWindowsFormsModule
+            // 
+            this.RequiredModuleTypes.Add(typeof(XAFDemo.Module.XAFDemoModule));
+            this.RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule));
+			this.RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.Scheduler.Win.SchedulerWindowsFormsModule));
+			this.RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.Validation.Win.ValidationWindowsFormsModule));
+        }
+
+        #endregion
+    }
+}
---
+++ XAFDemo.Module/BusinessObjects/CompanyContact.cs
@@ -0,0 +1,73 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace XAFDemo.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+    //[ImageName("BO_Contact")]
+    //[DefaultProperty("DisplayMemberNameForLookupEditorsOfThisType")]
+    //[DefaultListViewOptions(MasterDetailMode.ListViewOnly, false, NewItemRowPosition.None)]
+    //[Persistent("DatabaseTableName")]
+    // Specify more UI options using a declarative approach (https://documentation.devexpress.com/#eXpressAppFramework/CustomDocument112701).
+    public class CompanyContact : BaseObject
+    { // Inherit from a different class to provide a custom primary key, concurrency and deletion behavior, etc. (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument113146.aspx).
+        public CompanyContact(Session session)
+            : base(session)
+        {
+        }
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+            // Place your initialization code here (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112834.aspx).
+        }
+        private string _FirstName;
+        public string FirstName
+        {
+            get { return _FirstName; }
+            set { SetPropertyValue<string>(nameof(FirstName), ref _FirstName, value); }
+
+        }
+        private string _LastName;
+        public string LastName
+        {
+            get { return _FirstName; }
+            set { SetPropertyValue<string>(nameof(LastName), ref _LastName, value); }
+
+        }
+        private string _PhoneNumber;
+        public string PhoneNumber
+        {
+            get { return _PhoneNumber; }
+            set { SetPropertyValue<string>(nameof(PhoneNumber), ref _PhoneNumber, value); }
+
+        }
+
+        private string _Email;
+        public string Email
+        {
+            get { return _Email; }
+            set { SetPropertyValue<string>(nameof(Email), ref _Email, value); }
+
+        }
+        private Company _Company;
+        [Association]
+        public Company Company
+        {
+            get { return _Company; }
+            set { SetPropertyValue<Company>(nameof(Company), ref _Company, value); }
+        }
+
+        //}
+    }
+}
